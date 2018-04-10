9b423ab9c731b774d6073b31ef8e894523d63d3c
Cui <824760983@qq.com>
2018-04-10 18:00:46 +0800

    6-init

---
+++ src/com/gavin/problems/T0006.py
@@ -0,0 +1,13 @@
+# 6. ZigZag Conversion
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        return s
+
+
+s = Solution()
+s.convert("asfdasdfasf")
